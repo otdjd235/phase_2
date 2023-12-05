e9e24e1f526aaf1cc83eb3b059bff30ccafe8924
Dangar Othniel Djlouba <otdjd@ulaval.ca>
2023-12-04 23:08:34 -0500

    finalizing Erreur Date

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,6 @@
 """Ficher contenant les differents types d'erreurs  soulever"""
 class ErreurDate(RuntimeError):
     """Classe qui en capsule le message d'erreur Erreur date"""
+    def erreurdate(self):
+        """Fonction qui souleve le message d'erreur Erreur date """
+        raise ErreurDate
